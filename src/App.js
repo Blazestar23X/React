73d7bf51b8098ad4791d291a90476afd29fec1fc
Tony <tonyarakk@gmail.com>
2025-04-13 20:55:51 +1000

    Make rich feel bad

--- src/App.js
+++ src/App.js
@@ -38,6 +38,12 @@ function App() {
 	</div>
 	<h1 style={{color: colour}}>${count}</h1>
 	<h2 style={{color: colour}}>{previous}</h2>
+	if ({count}>200){
+		<h3> Wow your greedy. For that you lose everything. </h3>
+		setCount(0);
+	}else{
+		<h3> Maybe you could give a little to the homeless </h3>
+	};
       </header>
       <p>Third Message this will work</p>
     </div>
