44b1e5c02fe70fe377f4938079a80df7921c4292
Tony <tonyarakk@gmail.com>
2025-04-13 20:26:50 +1000

    Updates

---
+++ precache-manifest.3931417797d87d5ff9d27b0d2cde8643.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b06cce4725f5cdf3d0c8bb56efc308",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "49dfdfb6c22bd80b824c",
+    "url": "/React/static/css/main.73d73014.chunk.css"
+  },
+  {
+    "revision": "59fcf0b44b7c92bbd1e2",
+    "url": "/React/static/js/2.88d1964a.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/React/static/js/2.88d1964a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fe288e3ebf05321c803",
+    "url": "/React/static/js/3.863be910.chunk.js"
+  },
+  {
+    "revision": "49dfdfb6c22bd80b824c",
+    "url": "/React/static/js/main.469b4b4e.chunk.js"
+  },
+  {
+    "revision": "79fa89a7a8b6b3349325",
+    "url": "/React/static/js/runtime-main.8fd49b92.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/React/static/media/logo.06e73328.svg"
+  }
+]);
